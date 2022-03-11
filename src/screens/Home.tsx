cf850ecdae83e9447195524d95ce0b3ac0c09899
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-11 19:36:35 +0530

    get everything working

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,57 @@
 /** @format */
 
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import { http } from "../const/https";
 
 const Home = () => {
 	const [data, setData] = React.useState([]);
+	const [page, setPage] = React.useState(0);
+	const [showHeaderLoader, setShowHeaderLoader] = React.useState(false);
+	const [showFooterLoader, setShowFooterLoader] = React.useState(false);
 
 	React.useEffect(() => {
 		getData();
+		theTimer(true);
 	}, []);
 
-	function getData() {
+	function getData(toTop: Boolean = false) {
 		http
-			.get(`v1/search_by_date?tags=story&page=0`)
-			.then((res) => setData(res.data?.hits));
+			.get(`v1/search_by_date?tags=story&page=${page}`)
+			.then((res) => {
+				const theArray = toTop
+					? Array.from(new Set([...res.data.hits, ...data]))
+					: Array.from(new Set([...data, ...res.data.hits]));
+				setData(theArray);
+			})
+			.finally(() => {
+				setShowHeaderLoader(false);
+				setShowFooterLoader(false);
+			});
 	}
 
+	function incrementPage() {
+		setPage(page + 1);
+	}
+
+	function theTimer(toTop: Boolean = false) {
+		setInterval(() => {
+			setShowHeaderLoader(true);
+			incrementPage();
+			getData(toTop);
+		}, 10000);
+	}
 	return (
 		<View>
 			<FlatList
 				data={data}
+				ListHeaderComponent={() => {
+					if (showHeaderLoader) {
+						return <ActivityIndicator />;
+					} else {
+						return null;
+					}
+				}}
 				renderItem={({ item }) => (
 					<Text
 						style={{ margin: 5, padding: 5, backgroundColor: "rgba(0,0,0,.2)" }}
@@ -28,6 +59,18 @@ const Home = () => {
 						{item?.title}
 					</Text>
 				)}
+				onEndReached={() => {
+					incrementPage();
+					setShowFooterLoader(true);
+					getData();
+				}}
+				ListFooterComponent={() => {
+					if (showFooterLoader) {
+						return <ActivityIndicator />;
+					} else {
+						return null;
+					}
+				}}
 			/>
 		</View>
 	);
